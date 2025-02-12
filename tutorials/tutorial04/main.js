14a8003b74aa0d8db13907ca97853314f3de1961
Lucas <lucassimmons2005@gmail.com>
2025-02-12 13:43:55 -0500

    Updated Tutorial 4 output

--- tutorials/tutorial04/main.js
+++ tutorials/tutorial04/main.js
@@ -7,18 +7,18 @@ function setup() {
 
   // invoke any drawing functions inside of setup.
   // functions should all go between "createCanvas()" and "drawGrid()"
-  //draw5Circles();
-  // draw5RedSquares();
-  // draw5CirclesFor();
-  // drawNCircles(20);
+  draw5Circles();
+  draw5RedSquares();
+  draw5CirclesFor();
+  drawNCircles(20);
 
-  // drawNCirclesFlexible(30, 25, 400, 0);
-  // drawNCirclesFlexible(4, 100, 100, 200);
-  // drawNCirclesFlexible(8, 50, 700, 100);
+  drawNCirclesFlexible(30, 25, 400, 0);
+  drawNCirclesFlexible(4, 100, 100, 200);
+  drawNCirclesFlexible(8, 50, 700, 100);
 
-  // drawNShapesFlexible(30, 30, 335, 0, "square");
-  // drawNShapesFlexible(4, 100, 120, 200, "circle");
-  // drawNShapesFlexible(8, 50, 725, 25, "square");
+  drawNShapesFlexible(30, 30, 335, 0, "square");
+  drawNShapesFlexible(4, 100, 120, 200, "circle");
+  drawNShapesFlexible(8, 50, 725, 25, "square");
 
   drawNShapesDirectionFlexible(30, 30, 335, 0, "square", "column");
   drawNShapesDirectionFlexible(4, 100, 120, 200, "circle", "row");
@@ -28,46 +28,46 @@ function setup() {
 }
 
 // my first function
-// function draw5Circles() {
-//   noFill();
-//   // fill("red");
-//   let x = 100;
-//   let y = 100;
-//   let d = 10;
+function draw5Circles() {
+  noFill();
+  // fill("red");
+  let x = 100;
+  let y = 100;
+  let d = 10;
 
-//   let i = 0;
-//   while (i < 200) {
-//     circle(x, y + 10 * i, d++); // centerX, centerY, radius
-//     circle(x + 200, y + 10 * i, d++); // centerX, centerY, radius
+  let i = 0;
+  while (i < 200) {
+    circle(x, y + 10 * i, d++); // centerX, centerY, radius
+    circle(x + 200, y + 10 * i, d++); // centerX, centerY, radius
 
-//     if (i % 2 === 0) {
-//       fill("blue");
-//     } else {
-//       fill("red");
-//     }
-//     ++i;
-//   }
-// }
-// function draw5CirclesFor() {
-//   noFill();
-//   let x = 200;
-//   let y = 200;
-//   let d = 10;
+    if (i % 2 === 0) {
+      fill("blue");
+    } else {
+      fill("red");
+    }
+    ++i;
+  }
+}
+function draw5CirclesFor() {
+  noFill();
+  let x = 200;
+  let y = 200;
+  let d = 10;
 
-//   for (i = 0; i < 5; i++) {
-//     circle(x, y + 10 * i, d++); // centerX, centerY, radius
-//   }
-// }
-// function drawNCircles(n) {
-//   noFill();
-//   let x = 500;
-//   let y = 250;
-//   let d = 10;
+  for (i = 0; i < 5; i++) {
+    circle(x, y + 10 * i, d++); // centerX, centerY, radius
+  }
+}
+function drawNCircles(n) {
+  noFill();
+  let x = 500;
+  let y = 250;
+  let d = 10;
 
-//   for (i = 0; i < n; i++) {
-//     circle(x, y + 10 * i, d++); // centerX, centerY, radius
-//   }
-// }
+  for (i = 0; i < n; i++) {
+    circle(x, y + 10 * i, d++); // centerX, centerY, radius
+  }
+}
 
 function drawNCirclesFlexible(n, size, x, y) {
   noFill();
