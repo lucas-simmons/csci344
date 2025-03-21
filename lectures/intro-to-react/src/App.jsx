9d24b9a850b5df2808a05d7b1b658a5b29be4fc4
Lucas <lucassimmons2005@gmail.com>
2025-03-21 17:25:51 -0400

    Added homework 3 and changed styling of homepage

---
+++ lectures/intro-to-react/src/App.jsx
@@ -0,0 +1,16 @@
+import Profile from  "./Profile.jsx";
+import React from "react";
+
+ export default function App() {
+
+     return (
+         <><object data="" type=""></object>
+             <header>
+                 <h1>My First App</h1>
+             </header>
+             <main>
+                 <p>Hello React!</p>
+             </main>
+         </>
+     );
+ }
